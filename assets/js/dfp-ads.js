74801b39cf0f03519fbc125ac67b297488a35b43
Tom Shafer <tj@tjshafer.com>
2016-08-11 14:01:02 -0700

    polyfill
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- assets/js/dfp-ads.js
+++ assets/js/dfp-ads.js
@@ -59,6 +59,21 @@ googletag.cmd.push(function () {
         }
     }
 
+    if (!String.prototype.includes) {
+        String.prototype.includes = function(search, start) {
+            'use strict';
+            if (typeof start !== 'number') {
+                start = 0;
+            }
+
+            if (start + search.length > this.length) {
+                return false;
+            } else {
+                return this.indexOf(search, start) !== -1;
+            }
+        };
+    }
+
     /**
      * Loads Ad Position
      *
