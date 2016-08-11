cb239f1af36b5c9d9d886899ff8b8d0f9d797510
Tom Shafer <tj@tjshafer.com>
2016-08-11 14:14:23 -0700

    use pollifull
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- assets/js/dfp-ads.js
+++ assets/js/dfp-ads.js
@@ -115,7 +115,7 @@ googletag.cmd.push(function () {
 
         var map = googletag.sizeMapping();
 
-        if ($.inArray(position.sizes, 'fluid')) {
+        if (position.sizes.includes('fluid')) {
             return false;
         }
 
@@ -179,6 +179,38 @@ googletag.cmd.push(function () {
         }
     }
 
+    if (!Array.prototype.includes) {
+        Array.prototype.includes = function(searchElement /*, fromIndex*/) {
+            'use strict';
+            if (this == null) {
+                throw new TypeError('Array.prototype.includes called on null or undefined');
+            }
+
+            var O = Object(this);
+            var len = parseInt(O.length, 10) || 0;
+            if (len === 0) {
+                return false;
+            }
+            var n = parseInt(arguments[1], 10) || 0;
+            var k;
+            if (n >= 0) {
+                k = n;
+            } else {
+                k = len + n;
+                if (k < 0) {k = 0;}
+            }
+            var currentElement;
+            while (k < len) {
+                currentElement = O[k];
+                if (searchElement === currentElement ||
+                    (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+                    return true;
+                }
+                k++;
+            }
+            return false;
+        };
+    }
     /**
      * [resizer description]
      * @return {[type]} [description]
