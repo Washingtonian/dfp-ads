ed25727b0a9859910497b628dce01a63bd1ea2cb
Tom Shafer <tj@tjshafer.com>
2016-08-11 14:06:43 -0700

    polyfill
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- assets/js/dfp-ads.js
+++ assets/js/dfp-ads.js
@@ -59,21 +59,6 @@ googletag.cmd.push(function () {
         }
     }
 
-    if (!String.prototype.includes) {
-        String.prototype.includes = function(search, start) {
-            'use strict';
-            if (typeof start !== 'number') {
-                start = 0;
-            }
-
-            if (start + search.length > this.length) {
-                return false;
-            } else {
-                return this.indexOf(search, start) !== -1;
-            }
-        };
-    }
-
     /**
      * Loads Ad Position
      *
@@ -194,21 +179,38 @@ googletag.cmd.push(function () {
         }
     }
 
-    if (!String.prototype.includes) {
-        String.prototype.includes = function(search, start) {
+    if (!Array.prototype.includes) {
+        Array.prototype.includes = function(searchElement /*, fromIndex*/) {
             'use strict';
-            if (typeof start !== 'number') {
-                start = 0;
+            if (this == null) {
+                throw new TypeError('Array.prototype.includes called on null or undefined');
             }
 
-            if (start + search.length > this.length) {
+            var O = Object(this);
+            var len = parseInt(O.length, 10) || 0;
+            if (len === 0) {
                 return false;
-            } else {
-                return this.indexOf(search, start) !== -1;
             }
+            var n = parseInt(arguments[1], 10) || 0;
+            var k;
+            if (n >= 0) {
+                k = n;
+            } else {
+                k = len + n;
+                if (k < 0) {k = 0;}
+            }
+            var currentElement;
+            while (k < len) {
+                currentElement = O[k];
+                if (searchElement === currentElement ||
+                    (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+                    return true;
+                }
+                k++;
+            }
+            return false;
         };
     }
-
     /**
      * [resizer description]
      * @return {[type]} [description]
