22fe897dd600c980c8ef4c53c2cf9120b7a5988a
Chris Combs <ccombs@gmail.com>
2016-11-10 10:36:13 -0500

    bugs, robustness, logging

--- assets/js/dfp-ads.js
+++ assets/js/dfp-ads.js
@@ -89,12 +89,13 @@ googletag.cmd.push(function () {
    */
 
    function load_unloaded_ad_positions() {
+     var ad_pos, len;
 
-     for (ad_pos = 0, len = Object.keys(dfp_ad_slot_objects).length; ad_pos < len; ++ad_pos) {
+     for (ad_pos = 0, len = Object.keys(dfp_ad_slot_objects).length - 1; ad_pos < len; ++ad_pos) {
        var thePosition = dfp_ad_slot_objects[Object.keys(dfp_ad_slot_objects)[ad_pos]];
        try {
          if (thePosition.getResponseInformation() == undefined) {
-           googletag.pubads().refresh([thePosition]);
+           googletag.pubads().refresh(thePosition,{changeCorrelator: false});
           }
          } catch (err) {
 
@@ -116,7 +117,9 @@ googletag.cmd.push(function () {
             var theId = thePosition.getSlotElementId();
             if (document.getElementById(theId) === null) {
               console.log("deleting " + theId);
-              googletag.destroySlots([thePosition]);
+              if (!googletag.destroySlots([thePosition])) {
+                console.log("couldn't destroySlots");
+              };
               delete dfp_ad_slot_objects[Object.keys(dfp_ad_slot_objects)[ad_pos]];
             }
           } catch (err) {
@@ -144,7 +147,11 @@ googletag.cmd.push(function () {
                         position.sizes,
                         position.position_tag
                     ).addService(googletag.pubads());
-            } catch(err) {}
+                    console.log("defined slot " + position.ad_name + " in " + position.position_tag);
+            } catch(err) {
+              console.log("error defining ad slot ");
+              console.log(position);
+            }
           }
       }
 
