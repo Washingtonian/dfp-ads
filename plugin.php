720ba0bd5ccb9eb0f2144c765566f9893baf06db
Tom Shafer <tj@tjshafer.com>
2017-08-24 11:47:32 -0700

    Revert "point to root"
    This reverts commit 3ac8cbd19fcd5fe22116132c6b82e3515091925e.

--- plugin.php
+++ plugin.php
@@ -22,7 +22,7 @@
  * @subpackage        DFP-Ads
  */
 /* Autoload */
-require '/home/washingtonian/webroots/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 /* Includes */
 include 'includes/Functions.php';
 include 'includes/Globals_Container.php';
