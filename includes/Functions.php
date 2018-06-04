8a217793fd6a9197a76765e51e134838ed74e4aa
Chris Combs <ccombs@washingtonian.com>
2018-06-04 15:13:54 -0400

    prebid 1.13.0

--- includes/Functions.php
+++ includes/Functions.php
@@ -316,7 +316,7 @@ function inline_dfp_scripts()
 
       <!-- prebid.js main -->
 
-      <script async="async" src="/wp-content/plugins/dfp-ads/assets/js/prebid-1485535861.js"></script>
+      <script async="async" src="/wp-content/plugins/dfp-ads/assets/js/prebid_20180604191048.js"></script>
       <script>
       var PREBID_TIMEOUT = 2000;
       var pbjs = pbjs || {};
